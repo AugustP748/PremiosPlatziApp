b9c801516cc79b9023f451d89b99dc502e81846c
Augusto <augustoxx2015@gmail.com>
2022-12-05 17:52:02 -0300

    Se creo las tablas de la bd en models de polls

--- premiosplatziapp/polls/models.py
+++ premiosplatziapp/polls/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    questionText = models.CharField(max_length=100)
+    pub_date = models.DateTimeField("date published")
+
+class Choice(models.Model):
+    questionFK = models.ForeignKey(Question,on_delete=models.CASCADE)
+    choiceText = models.CharField(max_length=100)
+    votes = models.IntegerField(default=0)
