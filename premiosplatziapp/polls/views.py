880b445d85798a4153c9adce7eac4f6836eee35f
Augusto <augustoxx2015@gmail.com>
2022-12-07 22:31:34 -0300

    Se configuro admin, se agrego nuevas vistas pasando parametro por url

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -3,6 +3,17 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world!")
+    return HttpResponse("Estas en la pagina principal de platzi premios!")
 
 
+def detail(request,question_id):
+    return HttpResponse(f"Estás viendo la pregunta numero {question_id}")
+
+
+
+def results(request,question_id):
+    return HttpResponse(f"Estás viendo los resulltados de la pregunta {question_id}")
+
+    
+def vote(request,question_id):
+    return HttpResponse(f"Estás votando a la pregunta {question_id}")
