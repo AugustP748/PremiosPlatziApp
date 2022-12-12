6b3f3b9d573024715fbe66178e35ce2402474353
Augusto <augustoxx2015@gmail.com>
2022-12-12 15:29:43 -0300

    Se mejoro admin con filtros y otros detalles

--- premiosplatziapp/polls/admin.py
+++ premiosplatziapp/polls/admin.py
@@ -3,7 +3,16 @@ from .models import Question,Choice
 
 # Register your models here.
 
-# Make available models
+class ChoiceInLine(admin.StackedInline):
+    model = Choice
+    extra = 3
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date", "questionText"]
+    inlines = [ChoiceInLine]
+    list_display = ("questionText", "pub_date","was_published_recently" )
+    list_filter = ["pub_date"]
+    search_fields = ["questionText"]
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
